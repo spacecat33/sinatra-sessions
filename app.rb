d615b351b8adc38353cb85f758e97a88a55ec4bd
Janan Akkad <jananakkad@hotmail.co.uk>
2022-10-14 18:21:45 +0100

    commit tree b592788806cb9b632e572b6963f8616acee9f7b3

--- app.rb
+++ app.rb
@@ -19,12 +19,13 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = 'hello'
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
     end
+
   end
 
   get '/fetch' do
@@ -36,7 +37,7 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
@@ -51,7 +52,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
